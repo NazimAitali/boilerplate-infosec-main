d4d1bb1b63d3da1a6692bfbb2b50a27be4bbf292
NazimAitali <nazimaitali88@gmail.com>
2022-08-06 10:55:39 +0100

    frameguardxssFilter

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
-
+app.use(helmet.xssFilter());
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
