9e507a7dab9df91e7596c5ee928f6605b61ef16a
NazimAitali <nazimaitali88@gmail.com>
2022-08-06 12:12:07 +0100

    noCache-fix

--- myApp.js
+++ myApp.js
@@ -10,6 +10,7 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({defaultSrc :["'self'","'trusted-cdn.com'"]}));
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
