236904fccc5d0459cdb99492ff56926dde400f94
NazimAitali <nazimaitali88@gmail.com>
2022-08-06 10:39:46 +0100

    frameguard

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ const helmet = require("helmet");
 const express = require("express");
 const app = express();
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
 
 module.exports = app;
 const api = require("./server.js");
