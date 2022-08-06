b162c861bc4face713dd8e078b21a0bafac36ee0
NazimAitali <nazimaitali88@gmail.com>
2022-08-06 12:48:03 +0100

    parent

--- myApp.js
+++ myApp.js
@@ -2,20 +2,25 @@ const helmet = require("helmet");
 const express = require("express");
 const app = express();
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
-app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com'],
+  helmet({
+    frameguard: {
+      action: "deny",
     },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-cdn.com"],
+      },
+    },
+    dnsPrefetchControl: true,
+    hidePoweredBy: true,
+    xssFilter: true,
+    noSniff: true,
+    ieNoOpen: true,
+    hsts: { maxAge: ninetyDaysInSeconds, force: true },
+    noCache: true,
   })
 );
 module.exports = app;
