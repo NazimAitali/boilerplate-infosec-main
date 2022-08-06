83be077dff078e83531690ae0234d7d37b096744
NazimAitali <nazimaitali88@gmail.com>
2022-08-06 11:22:23 +0100

    hsts

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 const helmet = require("helmet");
 const express = require("express");
 const app = express();
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
